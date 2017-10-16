b3541b407c9f8791318a0f02165b9b020d495927
Aplomb Huang <huangat2@vcu.edu>
2017-10-15 22:16:10 -0400

    the OS_sim is the main progam

---
+++ OS_Sim/src/os_sim/OS_Sim.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package os_sim;
+
+/**
+ *
+ * @author APLOMB
+ */
+public class OS_Sim {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
