b64edb5dea86caeaa8cf921848008b3a67949f6e
Aplomb Huang <huangat2@vcu.edu>
2017-10-20 15:48:33 -0400

    Third commit: minor changes and some comments in main class

--- OS_Sim/src/OS_Sim.java
+++ OS_Sim/src/OS_Sim.java
@@ -1,11 +1,21 @@
 
 
 /**
- *
  * @author APLOMB
- */
-public class OS_Sim {
+ **/
 
- 
+/**
+* CMSC 312 Fall 2017
+* Programming Project
+* To simulate how a operation system handles input, processes etc.
+*
+**/
+
+public class OS_Sim {
+    
+    public static void main (String [] arg)
+    {
+        
+    }
     
 }
