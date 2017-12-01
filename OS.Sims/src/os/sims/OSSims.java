bff72ad768c04bb97cc9d48e9adad1567ec25fa8
Aplomb Huang <huangat2@vcu.edu>
2017-12-01 14:41:49 -0500

    something for last night

--- OS.Sims/src/os/sims/OSSims.java
+++ OS.Sims/src/os/sims/OSSims.java
@@ -22,7 +22,7 @@ public class OSSims {
 
     public static void main(String[] args) {
         
-       // OSmonitorIUG  osmonitor = new OSmonitorIUG();
+       //OSmonitorIUG  osmonitor = new OSmonitorIUG();
         
         int n = 0, io = 0, procCycle = 0, ioCycle = 0, cycles = 0;
         
@@ -42,7 +42,7 @@ public class OSSims {
      
      Thread1 t1 = new Thread1();
      
-     t1.run();
+    t1.run();
        
         
         System.out.println(cpu1.getThred());
