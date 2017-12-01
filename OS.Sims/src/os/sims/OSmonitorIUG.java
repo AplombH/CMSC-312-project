20cd90b1fac84e19dbb20696d6f871190d240da2
RodolfoTinoco <32085308+RodolfoTinoco@users.noreply.github.com>
2017-11-30 23:21:36 -0500

    Final version of the GUI

--- OS.Sims/src/os/sims/OSmonitorIUG.java
+++ OS.Sims/src/os/sims/OSmonitorIUG.java
@@ -16,168 +16,115 @@ import java.awt.event.ActionEvent;
 import java.util.StringTokenizer;
 import java.util.Random;
 
-public class OSmonitorIUG extends JFrame implements ActionListener {
 
-	private JPanel panelProcessTracker, panelCreate, panel3;
+public class OSmonitorIUG extends JFrame implements ActionListener
+{
 
-	private JTextField tfName, tfCPUtime, tfMainMem, tfParent;
-	private JTextField tfState;
+	private JPanel panel1, panel2, panel3;
 
-	private JButton bCreateP, bCreateIn, bTerminate, bExit;
-	private JButton bNewTxt, bCreateRandom, bConsult, bConsultByName;
+	private JTextField tfName, tfCPUtime, tfMainMem, tfParent, tfState;
+
+	private JButton bCreaP, bCreaIn, bTerminate, bExit;
+	private JButton bNewTxt, bCreaRand, bConsult, bConsultByName;
 	private JButton bReduceCPUtime, bConsByParentP;
-	private JTextArea taData;
+	private JTextArea taData, taMonitor;
 
 	private OSProcessAD processAD = new OSProcessAD();
-	// private JTextArea
+	//private JTextArea 
 
-	public OSmonitorIUG() {
+	public OSmonitorIUG()
+	{
 		super("Project OS monitor");
-		// 1 Creating Objects
+		//1 Creating Objects
 
-		panelProcessTracker = new JPanel();
-		panelCreate = new JPanel();
-		panel3 = new JPanel();
 
-		tfName = new JTextField();
-		tfCPUtime = new JTextField();
-		tfMainMem = new JTextField();
-		tfParent = new JTextField();
-		tfState = new JTextField();
+		//panelProcessTracker = new JPanel();
+		panel1 		= new JPanel();
+		panel2		= new JPanel();
+		panel3 		= new JPanel();
 
-		bCreateP = new JButton("Create Process");
-		bCreateIn = new JButton("Create Interrupt");
-		bTerminate = new JButton("Terminate");
-		bExit = new JButton("Exit Project");
-		bNewTxt = new JButton("Upload new txt");
-		bCreateRandom = new JButton("Create Random Processes");
+		tfName 		= new JTextField();
+		tfCPUtime	= new JTextField();
+		tfMainMem	= new JTextField();
+		tfParent	= new JTextField();
+		tfState		= new JTextField();
 
-		bConsult = new JButton("See all in MM");
+		bCreaP 		= new JButton("Create Process");
+		bCreaIn		= new JButton("Create Interrupt");
+		bTerminate	= new JButton("Terminate");
+		bCreaRand 	= new JButton("Create Random Processes");
+		bExit		= new JButton("Exit Project");
+		
+		bConsult 	= new JButton("See all in MM");
 		bConsultByName = new JButton("Search by Name");
 		bConsByParentP = new JButton("Search by Parent");
 		bReduceCPUtime = new JButton("Reduce CPUtime");
 
-		bCreateP.addActionListener(this);
-		bCreateIn.addActionListener(this);
+		bCreaP.addActionListener(this);
+		bCreaIn.addActionListener(this);
 		bTerminate.addActionListener(this);
 		bExit.addActionListener(this);
-		bNewTxt.addActionListener(this);
-		bCreateRandom.addActionListener(this);
-
+		bCreaRand.addActionListener(this);
 		bConsult.addActionListener(this);
 		bConsultByName.addActionListener(this);
 		bConsByParentP.addActionListener(this);
 		bReduceCPUtime.addActionListener(this);
 
-		taData = new JTextArea(10, 10);
+		taData = new JTextArea(10,10);
+		taMonitor = new JTextArea(32,50);
 
-		// 2 Adding them to the panel1 and panel2
-		panelProcessTracker.setLayout(new GridLayout(7, 7));
-		panelCreate.setLayout(new GridLayout(10, 2));
+
+		//2 Adding them to the panel1 and panel2
+		panel1.setLayout(new GridLayout(10,2));
+		panel2.setLayout(new GridLayout(2,1));
 		panel3.setLayout(new FlowLayout());
 
-		// panelProcessTracker
-		panelProcessTracker.add(new JLabel(":)"));
-		panelProcessTracker.add(new JLabel("P 1"));
-		panelProcessTracker.add(new JLabel("P 2"));
-		panelProcessTracker.add(new JLabel("P 3"));
-		panelProcessTracker.add(new JLabel("P 4"));
-		panelProcessTracker.add(new JLabel("P 5"));
-		panelProcessTracker.add(new JLabel("P 6"));
+		//panelCreate
 
-		panelProcessTracker.add(new JLabel("Name: "));
-		panelProcessTracker.add(new JLabel("P 1"));
-		panelProcessTracker.add(new JLabel("P 2"));
-		panelProcessTracker.add(new JLabel("P 3"));
-		panelProcessTracker.add(new JLabel("P 4"));
-		panelProcessTracker.add(new JLabel("P 5"));
-		panelProcessTracker.add(new JLabel("P 6"));
+		panel1.add(new JLabel("Name :"));
+		panel1.add(tfName);
 
-		panelProcessTracker.add(new JLabel("CPU: "));
-		panelProcessTracker.add(new JLabel("P 1"));
-		panelProcessTracker.add(new JLabel("P 2"));
-		panelProcessTracker.add(new JLabel("P 3"));
-		panelProcessTracker.add(new JLabel("P 4"));
-		panelProcessTracker.add(new JLabel("P 5"));
-		panelProcessTracker.add(new JLabel("P 6"));
+		panel1.add(new JLabel("CPU time"));
+		panel1.add(tfCPUtime);
 
-		panelProcessTracker.add(new JLabel("Main Memory: "));
-		panelProcessTracker.add(new JLabel("P 1"));
-		panelProcessTracker.add(new JLabel("P 2"));
-		panelProcessTracker.add(new JLabel("P 3"));
-		panelProcessTracker.add(new JLabel("P 4"));
-		panelProcessTracker.add(new JLabel("P 5"));
-		panelProcessTracker.add(new JLabel("P 6"));
+		panel1.add(new JLabel("Main Memory"));
+		panel1.add(tfMainMem);
 
-		panelProcessTracker.add(new JLabel("Parent Process: "));
-		panelProcessTracker.add(new JLabel("P 1"));
-		panelProcessTracker.add(new JLabel("P 2"));
-		panelProcessTracker.add(new JLabel("P 3"));
-		panelProcessTracker.add(new JLabel("P 4"));
-		panelProcessTracker.add(new JLabel("P 5"));
-		panelProcessTracker.add(new JLabel("P 6"));
+		panel1.add(new JLabel("Parent Process"));
+		panel1.add(tfParent);
 
-		panelProcessTracker.add(new JLabel("State: "));
-		panelProcessTracker.add(new JLabel("P 1"));
-		panelProcessTracker.add(new JLabel("P 2"));
-		panelProcessTracker.add(new JLabel("P 3"));
-		panelProcessTracker.add(new JLabel("P 4"));
-		panelProcessTracker.add(new JLabel("P 5"));
-		panelProcessTracker.add(new JLabel("P 6"));
+		panel1.add(new JLabel("State"));
+		panel1.add(tfState);
 
-		panelProcessTracker.add(new JLabel("Cycles Runned: "));
-		panelProcessTracker.add(new JLabel("P 1"));
-		panelProcessTracker.add(new JLabel("P 2"));
-		panelProcessTracker.add(new JLabel("P 3"));
-		panelProcessTracker.add(new JLabel("P 4"));
-		panelProcessTracker.add(new JLabel("P 5"));
-		panelProcessTracker.add(new JLabel("P 6"));
+		panel1.add(bCreaP);
+		panel1.add(bCreaIn);
+		panel1.add(bConsult);
+		panel1.add(bConsultByName);
+		panel1.add(bConsByParentP);
+		panel1.add(bTerminate);
+		panel1.add(bCreaRand);
+		panel1.add(bReduceCPUtime);
+		panel1.add(bExit); 
 
-		// panelCreate
+		//panel3.add(panelProcessTracker);
+		panel2.add(panel1);
+		panel2.add(new JScrollPane(taData));
 
-		panelCreate.add(new JLabel("Name :"));
-		panelCreate.add(tfName);
-
-		panelCreate.add(new JLabel("CPU time"));
-		panelCreate.add(tfCPUtime);
-
-		panelCreate.add(new JLabel("Main Memory"));
-		panelCreate.add(tfMainMem);
-
-		panelCreate.add(new JLabel("Parent Process"));
-		panelCreate.add(tfParent);
-
-		panelCreate.add(new JLabel("State"));
-		panelCreate.add(tfState);
-
-		panelCreate.add(bCreateP);
-		panelCreate.add(bCreateIn);
-		panelCreate.add(bTerminate);
-		panelCreate.add(bNewTxt);
-		panelCreate.add(bCreateRandom);
-		panelCreate.add(bExit);
-
-		panelCreate.add(bConsult);
-		panelCreate.add(bConsultByName);
-		panelCreate.add(bConsByParentP);
-		panelCreate.add(bReduceCPUtime);
-
-		// panel3.add(panelProcessTracker);
-		panel3.add(panelCreate);
-
-		panel3.add(new JScrollPane(taData));
+		panel3.add(panel2);
+		panel3.add(new JScrollPane(taMonitor));
 
 		taData.setText(processAD.obtainDataTxt());
 
-		// 3 Add panels to the JFrame and make it visible
+
+		//3 Add panels to the JFrame  and make it visible
 
 		add(panel3);
-		setSize(1100, 350);
+		setSize(1000,590);
 		setVisible(true);
 
 	}
 
-	private void edit(String data) {
+	private void edit(String data){
 		StringTokenizer st = new StringTokenizer(data, "_");
 
 		tfName.setText(st.nextToken());
@@ -187,82 +134,98 @@ public class OSmonitorIUG extends JFrame implements ActionListener {
 		tfState.setText(st.nextToken());
 	}
 
-	public String obtainData(boolean interrupt) {
-		String data = "";
+	public String obtainData(boolean interrupt)
+	{
+		String data="";
+			
+		String pName     	= tfName.getText();
+		String pCPUTime		= tfCPUtime.getText();
+		String pMainMem   	= tfMainMem.getText();
+		String pParent 		= tfParent.getText();
+		String pState		= tfState.getText();
 
-		String pName = tfName.getText();
-		String pCPUTime = tfCPUtime.getText();
-		String pMainMem = tfMainMem.getText();
-		String pParent = tfParent.getText();
-		String pState = tfState.getText();
-
-		if (pName.isEmpty() || pCPUTime.equals("") || pMainMem.equals("") || pParent.equals("") || pState.equals(""))
+		if(pName.isEmpty() || pCPUTime.equals("") || pMainMem.equals("") || pParent.equals("")||pState.equals(""))
 			data = "EMPTY_FIELD";
-		else {
-			try {
+		else
+		{
+			try
+			{
 				int n = Integer.parseInt(pCPUTime);
 				n = Integer.parseInt(pMainMem);
-				if (interrupt == false)
-					data = pName + "_" + pCPUTime + "_" + pMainMem + "_" + pParent + "_" + pState;
-				else {
-					data = pName + "_" + pCPUTime + "_" + pMainMem + "_" + "INTERRUPT" + "_" + pState;
+				if(interrupt == false)
+					data = pName+"_"+pCPUTime+"_"+pMainMem+"_"+pParent+"_"+pState;
+				else{
+					data = pName+"_"+pCPUTime+"_"+pMainMem+"_"+"INTERRUPT"+"_"+pState;
 				}
-			} catch (NumberFormatException nfe) {
+			}
+			catch(NumberFormatException nfe)
+			{
 				data = "NO_NUMERIC";
 			}
 		}
 		return data;
 	}
 
-	public void actionPerformed(ActionEvent e) {
+	public void refreshMonitor(){
+		String data = processAD.consult();
+		taMonitor.setText(data);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{
 		String data = "";
 		String answer = "";
 		String namProcess = "";
-
-		if (e.getSource() == bCreateP) {
+	
+		if(e.getSource() == bCreaP)
+		{
 			data = obtainData(false);
-
-			if (data.equals("EMPTY_FIELD"))
+			
+			if(data.equals("EMPTY_FIELD"))
 				taData.setText("Some field is empty...");
-			else {
-				if (data.equals("NO_NUMERIC"))
+			else
+			{
+				if(data.equals("NO_NUMERIC"))
 					taData.setText("Main Memory and CPU time must be numeric..");
-				else {
+				else
+				{
 					namProcess = tfName.getText();
 					answer = processAD.consultNameProcess(namProcess);
-
-					if (answer.equals("EMPTY"))
+					
+					if(answer.equals("EMPTY"))
 						answer = processAD.capture(data);
-					else {
-						if (answer.equals("NOT_FIND"))
+					else{
+						if(answer.equals("NOT_FIND"))
 							answer = processAD.capture(data);
 						else
-							answer = "The process already exist : " + namProcess;
+							answer = "The process already exist : "+namProcess;
 					}
 					taData.setText(answer);
 				}
 			}
 		}
 
-		if (e.getSource() == bCreateIn) {
+		if(e.getSource() == bCreaIn){
 			data = obtainData(true);
-
-			if (data.equals("EMPTY"))
+			
+			if(data.equals("EMPTY"))
 				taData.setText("Some field is empty...");
-			else {
-				if (data.equals("NO_NUMERIC"))
+			else
+			{
+				if(data.equals("NO_NUMERIC"))
 					taData.setText("Main Memory and CPU time must be numeric..");
-				else {
+				else
+				{
 					namProcess = tfName.getText();
 					answer = processAD.consultNameProcess(namProcess);
-
-					if (answer.equals("EMPTY"))
+					
+					if(answer.equals("EMPTY"))
 						answer = processAD.capture(data);
-					else {
-						if (answer.equals("NOT_FIND"))
+					else{
+						if(answer.equals("NOT_FIND"))
 							answer = processAD.capture(data);
 						else
-							answer = "The process already exist : " + namProcess;
+							answer = "The process already exist : "+namProcess;
 					}
 					taData.setText(answer);
 				}
@@ -270,58 +233,68 @@ public class OSmonitorIUG extends JFrame implements ActionListener {
 
 		}
 
-		if (e.getSource() == bConsult) {
+		if(e.getSource() == bConsult)
+		{
 			data = processAD.consult();
 			taData.setText(data);
 		}
 
-		if (e.getSource() == bConsultByName) {
+		if(e.getSource() == bConsultByName)
+		{
 			namProcess = tfName.getText();
 			data = processAD.consultNameProcess(namProcess);
 
-			if (data.equals("EMPTY"))
+			if(data.equals("EMPTY"))
 				data = "There are no processes in MM";
-			else if (data.equals("NOT_FIND"))
-				data = "NO process with that name is found: " + namProcess;
 			else
-				edit(data);
+				if(data.equals("NOT_FIND"))
+					data = "NO process with that name is found: " +namProcess;
+				else
+					edit(data);
 			taData.setText(data);
 		}
 
-		if (e.getSource() == bConsByParentP) {
+		if(e.getSource() == bConsByParentP)
+		{
 			data = tfParent.getText();
 			data = processAD.consultParentProcess(data);
 			taData.setText(data);
 		}
 
-		if (e.getSource() == bReduceCPUtime) {
+		if(e.getSource() == bReduceCPUtime)
+		{
 			namProcess = tfName.getText();
 			data = processAD.consultNameProcess(namProcess);
 
-			if (data.equals("EMPTY"))
+			if(data.equals("EMPTY"))
 				data = "No Process on MM";
-			else if (data.equals("NOT_FIND"))
-				data = "The process was not localizated";
-			else {
-				data = processAD.reduceCPUtime(0);
-			}
+			else
+				if(data.equals("NOT_FIND"))
+					data = "The process was not localizated";
+				else
+				{
+					data = processAD.reduceCPUtime(0);
+				}
 			taData.setText(data);
 		}
 
-		if (e.getSource() == bTerminate) {
+		if(e.getSource() == bTerminate)
+		{
 			namProcess = tfName.getText();
 			data = processAD.consultNameProcess(namProcess);
 
-			if (data.equals("EMPTY"))
+			if(data.equals("EMPTY"))
 				data = "The Main Memory is empty";
-			else if (data.equals("NOT_FIND"))
-				data = "THe process was not found" + namProcess;
 			else
-				data = processAD.terminateProcess();
-			taData.setText(data);
+				if(data.equals("NOT_FIND"))
+					data = "THe process was not found" + namProcess;
+				else
+					data = processAD.terminateProcess();
+			taData.setText(data); 
 		}
 
-		if (e.getSource() == bCreateRandom) {
+		if(e.getSource() == bCreaRand)
+		{
 			Random rand = new Random();
 			//int randNum = rand.nextInt(10) + 1;
 			int randpName;
@@ -348,17 +321,19 @@ public class OSmonitorIUG extends JFrame implements ActionListener {
 			
 			data = "process" +randpName + "_" + randpCPUtime + "_" + randpMainMem + "_parent" + randpParent + "_" + randpState;
 			processAD.capture(data);
+			
 		}
 
-		if (e.getSource() == bExit) {
-			// answer = processAD.save();
+		if(e.getSource() == bExit)
+		{
 			System.exit(0);
 		}
 	}
 
-	public static void main(String args[]) {
-		OSmonitorIUG osmonitor = new OSmonitorIUG();
-	}
 
+
+	public static void main(String args[]){
+		OSmonitorIUG  osmonitor = new OSmonitorIUG();
+	}
 
 }
